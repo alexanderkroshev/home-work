7f366042554c7fd3c98411c73fbcd3bf652e0ba9
alexanderkroshev <alexanderkroshev@gmail.com>
2022-01-11 21:23:17 +0700

    add method updateNumberByClientId

--- src/test/java/com/sbrf/reboot/repository/FileAccountRepositoryTest.java
+++ src/test/java/com/sbrf/reboot/repository/FileAccountRepositoryTest.java
@@ -3,6 +3,7 @@ package com.sbrf.reboot.repository;
 import com.sbrf.reboot.AccountRepository;
 import com.sbrf.reboot.repository.impl.FileAccountRepository;
 import org.junit.jupiter.api.Test;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,27 +13,36 @@ class FileAccountRepositoryTest {
 
     AccountRepository accountRepository;
 
+
     @Test
-    void onlyPersonalAccounts()  {
+    void onlyPersonalAccounts() {
         String filePath = "src/main/resources/Accounts.txt";
         accountRepository = new FileAccountRepository(filePath);
-
         long clientId = 1L;
         Set<Long> actualAccounts = accountRepository.getAllAccountsByClientId(clientId);
-
         Set<Long> expected = new HashSet<Long>() {{
             add(111L);
             add(222L);
             add(333L);
         }};
-
         actualAccounts.forEach(e -> assertTrue(expected.contains(e)));
     }
 
-  @Test
+    @Test
     void updateNumberByClientId() {
+        String filePath = "src/main/resources/Accounts.txt";
+        FileAccountRepository repository = new FileAccountRepository(filePath);
+        long clientId = 1L;
+        repository.updateNumberByClientId(clientId, 333L, 444L);
+        Set<Long> accounts = repository.getAllAccountsByClientId(clientId);
 
-
+        Set<Long> expected = new HashSet<Long>() {{
+            add(111L);
+            add(222L);
+            add(444L);
+        }};
+        accounts.forEach(e -> assertTrue(expected.contains(e)));
+        repository.updateNumberByClientId(clientId, 444L, 333L);
     }
 
 }
