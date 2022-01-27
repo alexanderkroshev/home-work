468890f11e2ad9cbd4794f039302cfdb6e5b8154
alexanderkroshev <alexanderkroshev@gmail.com>
2022-01-27 14:31:34 +0700

    initial commit

--- src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
+++ src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
@@ -1,37 +1,64 @@
 package com.sbrf.reboot.concurrentmodify;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.ConcurrentModificationException;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RemoveElementWithoutErrorsTest {
 
+    List<Integer> list = new ArrayList(Arrays.asList(1, 2, 3));
+    List<Integer> list2 = new ArrayList(Arrays.asList(2, 3));
+
+    @AfterEach
+    void init() {
+        list = new ArrayList(Arrays.asList(1, 2, 3));
+    }
+
     @Test
     public void successConcurrentModificationException() {
 
-        List<Integer> list = new ArrayList() {{
-            add(1);
-            add(2);
-            add(3);
-        }};
-
         assertThrows(ConcurrentModificationException.class, () -> {
-            
-            for (Integer integer : list) {
+
+            for ( Integer integer : list ) {
                 list.remove(1);
             }
-            
+
         });
 
     }
 
     @Test
     public void successRemoveElementWithoutErrors() {
+        Integer integer;
+        Iterator<Integer> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            integer =  iterator.next();
+            if (integer == 1)
+                iterator.remove();
+        }
 
+        assertEquals(list, list2);
+    }
+
+    @Test
+    public void successRemoveElementWithoutErrors2() {
+
+        list.removeIf(i -> i == 1);
+
+        assertEquals(list, list2);
+    }
+
+    @Test
+    public void successRemoveElementWithoutErrors3() {
+
+        list = list.stream().filter(i -> i != 1).collect(Collectors.toList());
+
+        assertEquals(list, list2);
     }
 
 }
