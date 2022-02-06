418b03d07bdfdaaf1f56f4323c3a7cff420e1e33
alexanderkroshev <alexanderkroshev@gmail.com>
2022-02-06 12:49:39 +0700

    additional tests

--- src/test/java/com/sbrf/reboot/streams/StreamTest.java
+++ src/test/java/com/sbrf/reboot/streams/StreamTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
